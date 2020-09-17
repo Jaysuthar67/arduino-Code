0c540f172f714e12dcea6a9674f5ace81689fba1
jaysuthar67 <jaysuthar67@gmail.com>
2020-09-17 13:51:29 +0530

    libadded

--- ESP-Client-Control-Lib/FS_DEFAULT_JSON/FS_DEFAULT_JSON.ino
+++ ESP-Client-Control-Lib/FS_DEFAULT_JSON/FS_DEFAULT_JSON.ino
@@ -1,12 +1,36 @@
+#include <LittleFS.h>
 #include <ArduinoJson.h>
-#include <FS.h>
 
 void setup() {
-  SPIFFS.format();
+  LittleFS.format();
   Serial.begin(115200);
   Serial.println("");
   StaticJsonDocument<200> doc;
   doc["hello"] = "world";
-  serializeJson(doc, Serial);
+  doc["ada"] = "wwfaorld";
+  doc["ab3ge"] = "eg";
+  doc["adbw34sd"] = "aeg";
+  LittleFS.begin();
+  if (LittleFS.mkdir("root")) {
+    File fptr = LittleFS.open("/root/hello.json", "w+");
+    if (!fptr) {
+      Serial.println("file open failed");
+    } else {
+      serializeJson(doc, fptr);
+      fptr.close();
+    }
+  }
+
+  File fptr2 = LittleFS.open("/root/hello.json", "r");
+  if (!fptr2) {
+    Serial.println("file open failed | fptr2");
+  } else {
+    DynamicJsonDocument doc1(2048);
+    deserializeJson(doc1, fptr2);
+    String str = doc1["hello"];
+    Serial.println(str);
+    fptr2.close();
+  }
+  LittleFS.end();
 }
 void loop() {}
